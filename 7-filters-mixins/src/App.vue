fb03f5762dfb5ebd94dff81b4e5075c1a0da2376
ChidinmaKO <kchidinmavictoria@gmail.com>
2020-03-25 19:08:48 +0100

    add filters

--- 7-filters-mixins/src/App.vue
+++ 7-filters-mixins/src/App.vue
@@ -3,17 +3,63 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
             </div>
         </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h3>Filters</h3>
+                <p>To Upper Case: {{ text | toUpperCase }}</p>
+                <p>To Lower Case: {{ text | toUpperCase | toLowerCase }}</p>
+                <p>Capitalize: {{ secondText | capitalize }}</p>
+
+                <hr>
+
+                <input v-model="filterText">
+
+                <p>List of Edible Berries</p>
+                <ul>
+                    <li v-for="(berry, index) in filteredBerries" :key="`berry[1]-${index}`">{{ berry }}</li>
+                </ul>
+            </div>
+        </div>
+
+        <hr>
+
     </div>
 </template>
 
 <script>
     export default {
+        filters: {
+            capitalize(value) {
+                return !value ? '' : value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+            }
+        },
+        data() {
+            return {
+                text: 'Hello there',
+                secondText: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                filterText: '',
+                berries: [
+                            'Raspberry', 'Strawberry', 'Blackberry', 'Blueberry', 
+                            'Goji berry', 'Bilberry', 'Acai berry', 'Cranberry', 
+                            'Elderberry', 'Cloudberry', 'Huckleberry', 'Gooseberry',
+                            'Chokeberry', 'Mulberry', 'Salmonberry', 'Saskatoon berry',
+                            'Buffaloberry', 'Muscadine berry'
+                        ],
+            }
+        },
+        computed: {
+            filteredBerries() {
+                return this.berries.filter((berry) => {
+                    return berry.toLowerCase().match(this.filterText);
+                })
+            }
+        },
     }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- 7-filters-mixins/src/main.js
+++ 7-filters-mixins/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('toUpperCase', (value) => {
+  return !value ? '' : value.toUpperCase();
+})
+Vue.filter('toLowerCase', function(value) {
+  return !value ? '' : value.toLowerCase();
+})
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
