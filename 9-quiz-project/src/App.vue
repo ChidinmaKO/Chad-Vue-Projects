9fc09f71a9c72e43f5be5b8cc48ba4022d1d30bf
ChidinmaKO <kchidinmavictoria@gmail.com>
2020-03-29 05:16:13 +0200

    conclude quiz project

--- 9-quiz-project/src/App.vue
+++ 9-quiz-project/src/App.vue
@@ -5,12 +5,15 @@
                 <h1 class="text-center">The Super Quiz</h1>
             </div>
         </div>
+
         <hr>
+
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition name="flip" mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
+                </transition>
+
             </div>
         </div>
     </div>
@@ -28,12 +31,7 @@
         },
         methods: {
           answered(isCorrect) {
-              if (isCorrect) {
-                  this.mode = 'app-answer';
-              } else {
-                  this.mode = 'app-question';
-                  alert('Wrong, try again!');
-              }
+            return isCorrect ? this.mode = 'app-answer' : alert('Wrong, try again!');
           }
         },
         components: {
@@ -42,4 +40,33 @@
         }
     }
 </script>
+
+<style scoped>
+
+    .flip-enter-active {
+        animation: flip-in .5s ease-out forwards;
+    }
+
+    .flip-leave-active {
+        animation: flip-out .5s ease-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0);
+        }
+    }
+</style>
 
